edad3737ad138b25b4e7f0e1a8c3abdb61ad5580
sfi4l <cryptosfi4l@gmail.com>
2023-10-22 08:56:27 +0300

    update chat and smart

--- src/components/chat/index.jsx
+++ src/components/chat/index.jsx
@@ -1,49 +1,60 @@
-import { useState } from "react"
-import styles from "./style.module.css"
-import { useLocalStorage } from "../hook/useLocalStorage"
-import {Link} from "react-router-dom"
+import { useState } from "react";
+import styles from "./style.module.css";
+import { useLocalStorage } from "../hook/useLocalStorage";
+import { Link } from "react-router-dom";
 
 const Chat = () => {
-    const [chatDate,setChatDate] = useLocalStorage("CHat",[])
-    const [inputValue,setInputValue] = useState("")
-    const [user] = useLocalStorage("USER")
-    const  [showDialogReg,setShowDialogReg] = useState(false)
-    
-    const changeChat = () => {
-if(user){
+  const [chatDate, setChatDate] = useLocalStorage("Chat", []);
+  const [inputValue, setInputValue] = useState("");
+  const [user] = useLocalStorage("USER");
+  const [showDialogReg, setShowDialogReg] = useState(false);
 
-    setChatDate([...chatDate,`${user? user:"Аноним"}:${inputValue}`]);
-    setInputValue("")
-}
-else{
-    setShowDialogReg(true)
-    setInputValue("")
-
-}
+  const changeChat = () => {
+    if (user) {
+      if (inputValue.trim() !== "") {
+        setChatDate([...chatDate, `${user ? user : "Аноним"}: ${inputValue}`]);
+        setInputValue("");
+      }
+    } else {
+      setShowDialogReg(true);
+      setInputValue("");
     }
-    return <div>
-        <div className={styles.contChat}>
-            <div className={styles.title}>Чат</div>
-            {chatDate.map((el,i)=>{
-                return(
-                    <div key={i}>
-                        {i + 1}.{el}
+  };
 
-                    </div>
-                )
-            })}
+  return (
+    <div className={styles.chatContainer}>
+      <div className={styles.title}>Чат</div>
+      <div className={styles.chatContent}>
+        {chatDate.map((el, i) => {
+          return (
+            <div key={i} className={styles.chatMessage}>
+              {i + 1}.{el}
+            </div>
+          );
+        })}
+      </div>
+      <div className={styles.contInpBtn}>
+        <input
+          type="text"
+          value={inputValue}
+          className={styles.inpBtn}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Напишите комментарий"
+        />
+        <button onClick={changeChat} className={styles.inpBtn}>
+          Отправить
+        </button>
+        <button onClick={() => setChatDate([])} className={styles.inpBtn}>
+          Очистить чат
+        </button>
+      </div>
+      {showDialogReg && (
+        <div className={styles.title}>
+          <Link to="/">Вы не авторизованы</Link>
         </div>
-        <div className={styles.contInpBtn}>
-            
-            <input type="text" value={inputValue}  className={styles.inpBtn}onChange={(e)=> setInputValue(e.target.value)} placeholder="Напишите комментарий" />
-            <button onClick={changeChat} className={styles.inpBtn}>Отправить</button>
-            <button onClick={()=>setChatDate([])} className={styles.inpBtn}>Очистить чат</button>
-            
-        </div>
-        {showDialogReg && (
-                <div className={styles.title} ><Link to="/">Вы не авторизованы</Link></div>
-            )}
+      )}
     </div>
-}
+  );
+};
 
-export default Chat
+export default Chat;
--- src/components/smart/index.jsx
+++ src/components/smart/index.jsx
@@ -1,91 +1,140 @@
-import { useState } from "react"
-import style from "./style.module.css"
-import bin from "../../image/bin.png"
-import on from "../../image/on.png"
-import off from "../../image/off.png"
-import edit from "../../image/edit.png"
-import save from "../../image/save.png"
-import { useLocalStorage } from "../hook/useLocalStorage"
+import { useState } from "react";
+import style from "./style.module.css";
+import bin from "../../image/bin.png";
+import on from "../../image/on.png";
+import off from "../../image/off.png";
+import edit from "../../image/edit.png";
+import save from "../../image/save.png";
+import { useLocalStorage } from "../hook/useLocalStorage";
+
 const SmartHouse = () => {
-    
-    const [device,setDevice] = useLocalStorage("smart",[
-        {id:1, name:"Лампочка",isPower:false,isEdit:false},
-        {id:2, name:"Телевизор",isPower:false,isEdit:false},
-        {id:3, name:"Колонка" ,isPower:false,isEdit:false},
-        {id:4, name:"Компьютер" ,isPower:false,isEdit:false},
-    ])
-    const [addValue,setAddValue] = useState("")
-    const [editValue,setEditValue]=useState("")
-    const handleAdd = () => {
-        setDevice([...device,{id:Date.now, name:addValue,isPower:false,isEdit:false}])
-        setAddValue("");
-    }
-    const handleDelete = (idDevice) => {
-        setDevice(()=> device.filter((el)=>el.id !== idDevice))
+  const [device, setDevice] = useLocalStorage("smart", [
+    { id: 1, name: "Лампочка", isPower: false, isEdit: false },
+    { id: 2, name: "Телевизор", isPower: false, isEdit: false },
+    { id: 3, name: "Колонка", isPower: false, isEdit: false },
+    { id: 4, name: "Компьютер", isPower: false, isEdit: false },
+  ]);
+  const [addValue, setAddValue] = useState("");
+  const [editValue, setEditValue] = useState("");
 
+  const handleAdd = () => {
+    if (addValue.trim() === "") {
+      // Не добавлять пустые устройства
+      return;
     }
+    setDevice([
+      ...device,
+      { id: Date.now(), name: addValue, isPower: false, isEdit: false },
+    ]);
+    setAddValue("");
+  };
 
-    const handlePower = (idDevice) => {
-        setDevice(()=> device.map(el=>{
-            if(el.id === idDevice){
-                return {...el,isPower:!el.isPower} 
-            }
-            return el;
-        }))
+  const handleDelete = (idDevice) => {
+    setDevice(() => device.filter((el) => el.id !== idDevice));
+  };
+
+  const handlePower = (idDevice) => {
+    setDevice(() =>
+      device.map((el) => {
+        if (el.id === idDevice) {
+          return { ...el, isPower: !el.isPower };
+        }
+        return el;
+      })
+    );
+  };
+
+  const showEditDialog = (isDevice) => {
+    setDevice(() =>
+      device.map((el) => {
+        if (el.id === isDevice) {
+          setEditValue(el.name);
+          return { ...el, isEdit: true };
+        }
+        return el;
+      })
+    );
+  };
+
+  const handleEdit = (isDevice) => {
+    if (editValue.trim() === "") {
+      // Не редактировать устройство с пустым названием
+      return;
     }
-    const showEditDialog = (isDevice) => {
-        setDevice(()=> device.map(el=>{
-            if(el.id === isDevice){
-                setEditValue(el.name)
-                return{...el,isEdit:true};
-                
-            }
-            return el
-        }))
-    }
-    const handleEdit = (isDevice)=> {
-        setDevice(()=> device.map(el=>{
-            if(el.id === isDevice){
-                return{...el,name:editValue,isEdit:false}
-            }
-            return el;
-        }))
+    setDevice(() =>
+      device.map((el) => {
+        if (el.id === isDevice) {
+          return { ...el, name: editValue, isEdit: false };
+        }
+        return el;
+      })
+    );
+  };
 
-    }
-    return <div className={style.cont}>
-        <div className={style.title}>Устройства умного дома</div>
-
-        <div>
-    <input type="text" value={addValue} className={style.inputlog} onChange={(e)=> setAddValue(e.target.value)} placeholder="Введите название устройства" />
-    <button onClick={handleAdd} className={style.btn}>Добавить устройство</button>
-
-        </div>
-        <div>{device.map( (item) => {
-            return(<div>
-                {item.isEdit ? (
-                    <div>
-                      <input type="text" value={editValue} onChange={(e)=> setEditValue(e.target.value)} className={style.inputedit} placeholder="Введите новое название" /> 
-                      <img onClick={()=>handleEdit(item.id)} src={save} className={style.pic} alt="save" />
-                    </div>
-                ): <div className={style.card} key={item.id}>
-                <div className={style.title}>
-                    {item.name}
-                </div>
+  return (
+    <div className={style.cont}>
+      <div className={style.title}>Устройства умного дома</div>
+      <div>
+        <input
+          type="text"
+          value={addValue}
+          className={style.inputlog}
+          onChange={(e) => setAddValue(e.target.value)}
+          placeholder="Введите название устройства"
+        />
+        <button onClick={handleAdd} className={style.btn}>
+          Добавить устройство
+        </button>
+      </div>
+      <div>
+        {device.map((item) => (
+          <div>
+            {item.isEdit ? (
+              <div>
+                <input
+                  type="text"
+                  value={editValue}
+                  onChange={(e) => setEditValue(e.target.value)}
+                  className={style.inputedit}
+                  placeholder="Введите новое название"
+                />
+                <img
+                  onClick={() => handleEdit(item.id)}
+                  src={save}
+                  className={style.pic}
+                  alt="save"
+                />
+              </div>
+            ) : (
+              <div className={style.card} key={item.id}>
+                <div className={style.title}>{item.name}</div>
                 <div className={style.picMap}>
-                    <img onClick={() => handlePower(item.id)} src={item.isPower ? on:off } className={style.pic} alt="on/off" />
-                     <img onClick={()=>showEditDialog(item.id)} src={edit} className={style.pic} alt="edit"/>
-                    <img src={bin} onClick={()=> handleDelete(item.id)} className={style.pic}alt="delete" />
+                  <img
+                    onClick={() => handlePower(item.id)}
+                    src={item.isPower ? on : off}
+                    className={style.pic}
+                    alt="on/off"
+                  />
+                  <img
+                    onClick={() => showEditDialog(item.id)}
+                    src={edit}
+                    className={style.pic}
+                    alt="edit"
+                  />
+                  <img
+                    src={bin}
+                    onClick={() => handleDelete(item.id)}
+                    className={style.pic}
+                    alt="delete"
+                  />
                 </div>
-                    </div>}
-            </div>
-
-    
-
-
-           
-        )
-        })}</div>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
-}
+  );
+};
 
-export default SmartHouse
+export default SmartHouse;
